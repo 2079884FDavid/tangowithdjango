421c1cf62ccbd913d843fa4f75fe98c99c1c7a27
David Froehlingsdorf <2079884F@student.gla.ac.uk>
2015-03-06 17:05:59 +0000

    Chapter 16 and 17 - Completed

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -12,6 +12,11 @@ urlpatterns = patterns('',
     url(r'^restricted/', views.restricted, name='restricted'),
     #url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^search/$', views.search, name='search'),
+    url(r'^goto/$', views.track_url, name='goto'),
+    url(r'^add_profile/$', views.register_profile, name='add_profile'),
+    url(r'^profile/(?P<profile_user_name>[\w\-]+)/$', views.profile, name='profile'),
+    url(r'^edit_profile/$', views.edit_profile, name='edit_profile'),
+    url(r'^profile_list/$', views.profile_list, name='profile_list'),
     ) 
 
 
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.models import UserProfile
 from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
@@ -59,6 +61,12 @@ def category(request, category_name_slug):
 
     
     context_dict = {}
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            context_dict['result_list'] = run_query(query)
 
     try:
         
@@ -132,6 +140,149 @@ def search(request):
             result_list = run_query(query)
 
     return render(request, 'rango/search.html', {'result_list': result_list})
+
+def track_url(request):
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+            try:
+                page = Page.objects.get(title=page_id)
+                page.views = page.views+1
+                #The associated category needs to be updated as well
+                cat = Category.objects.get(name=page.category)
+                cat.views = cat.views + 1
+                cat.save()
+                page.save()
+                return redirect(page.url)
+            except:
+                print "Page does not exists"
+
+    return redirect('/rango/')
+
+@login_required
+def register_profile(request):
+    current_user = request.user
+    try:
+        UserProfile.objects.get(user=current_user)
+        # This user has already a profile
+        return HttpResponseRedirect('/rango/')
+    
+    except UserProfile.DoesNotExist:
+        if request.method == 'POST':
+            profile_form = UserProfileForm(data=request.POST)
+
+            if profile_form.is_valid():
+
+                profile = profile_form.save(commit=False)
+                profile.user = current_user
+
+                if 'picture' in request.FILES:
+                    profile.picture = request.FILES['picture']
+
+                profile.save()
+            else:
+                print profile_form.errors
+            return HttpResponseRedirect('/rango/')
+        else:
+            profile_form = UserProfileForm()
+
+        return render(request,
+                'rango/profile_registration.html',
+                {'profile_form': profile_form} )
+
+@login_required
+def profile(request, profile_user_name):
+    context_dict = {}
+    try:
+        user = User.objects.get(username=profile_user_name)
+        context_dict['requested_user'] = user
+        try:
+            profile = UserProfile.objects.get(user=user)
+            context_dict['profile_exists'] = True
+            context_dict['profile'] = profile
+        except UserProfile.DoesNotExist:
+            context_dict['profile_exists'] = False
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/profile.html', context_dict)
+
+@login_required
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        field = user_form.fields['email']
+        data = field.widget.value_from_datadict(user_form.data, user_form.files, user_form.add_prefix('email'))
+        try:
+            current_user.email = field.clean(data)
+            valid_update = True
+        except:
+            valid_update = False
+        current_user.save(update_fields=['email'])
+
+        try:
+            profile = UserProfile.objects.get(user=current_user)
+            # This user has already a profile
+            field = profile_form.fields['website']
+            data = field.widget.value_from_datadict(profile_form.data, profile_form.files, profile_form.add_prefix('website'))
+            try:
+                profile.website = field.clean(data)
+            except:
+                valid_update = False
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            
+    
+        except UserProfile.DoesNotExist:
+            profile_form = UserProfileForm(data=request.POST)
+
+            if profile_form.is_valid():
+
+                profile = profile_form.save(commit=False)
+                profile.user = current_user
+
+                if 'picture' in request.FILES:
+                    profile.picture = request.FILES['picture']
+
+                profile.save()
+            else:
+                print profile_form.errors
+                valid_update = False
+
+        if valid_update:
+            return HttpResponseRedirect('/rango/profile/'+current_user.username+'/')
+
+
+    context_dict = {}
+    user_form = UserForm(initial={'email': current_user.email})
+    try:
+        profile = UserProfile.objects.get(user=current_user)
+        context_dict['profile_exists'] = True
+        context_dict['profile'] = profile
+        profile_form = UserProfileForm(initial={'website': profile.website})
+    except UserProfile.DoesNotExist:
+        context_dict['profile_exists'] = False
+        profile_form = UserProfileForm()
+
+    context_dict['user_form'] = user_form
+    context_dict['profile_form'] = profile_form
+
+    return render(request,
+            'rango/edit_profile.html',
+            context_dict)
+
+
+@login_required
+def profile_list(request):
+    context_dict = {}
+    try:  
+        users = User.objects.filter()
+        context_dict['users'] = users
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/profile_list.html', context_dict)
 
 
 ##def register(request):
